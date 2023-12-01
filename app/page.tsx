6eab544726e84a4d2493defa258dcad0bec92216
tuanthinh16 <tuanthinhdz@gmail.com>
2023-12-01 23:12:01 +0700

    product

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import CarouselComponent from '@/components/UI/app.carousel';
 import React from 'react';
 import axios from 'axios';
 import ProductItem from '@/components/ITEM/item.Product';
+import Loading from './loading';
 
 export default function Home() {
   const [product,setProduct] = React.useState<any[]>([]);
@@ -34,7 +35,7 @@ export default function Home() {
       <h2>Product</h2>
         <div style={{display:'flex',flexWrap:'wrap'}}>
           
-          <ProductItem product={product}/>
+          <ProductItem product={product} fallback={<Loading/>}/>
         </div>
       </div>
     </Container>
--- app/product/page.tsx
+++ app/product/page.tsx
@@ -3,11 +3,12 @@ import ProductItem from '@/components/ITEM/item.Product';
 import axios from 'axios';
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import Loading from './loading';
 
 const Product = () => {
     const [product,setProduct] = React.useState<any[]>([]);
     React.useEffect(()=>{
-        document.title = "Bunney | Home";
+        document.title = "Bunney | Product";
         fetchData();
     },[]);
     const fetchData =()=>{
@@ -24,7 +25,7 @@ const Product = () => {
         <Container>
             <h1>All Products</h1>
             <div style={{display:'flex',flexWrap:'wrap'}}>
-            <ProductItem product={product}/>
+            <ProductItem product={product} fallback={<Loading/>}/>
             </div>
         </Container>
     )
